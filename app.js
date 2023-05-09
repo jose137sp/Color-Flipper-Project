38dffe45444ef563f29631960acd829db6d79e31
Jose <jose137sp@gmail.com>
2023-05-08 19:51:46 -0500

    functionality with the JS script

---
+++ app.js
@@ -0,0 +1,13 @@
+// Seleccionamos el botón y el label
+const button = document.getElementById('btn');
+const colorCode = document.getElementById('color-code');
+
+// Agregamos el evento click al botón
+button.addEventListener('click', function () {
+    // Generamos un color aleatorio en formato hexadecimal
+    const color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    // Cambiamos el color del fondo del botón
+    button.style.backgroundColor = color;
+    // Actualizamos el valor del label con el código hexadecimal del color generado
+    colorCode.textContent = color;
+});
