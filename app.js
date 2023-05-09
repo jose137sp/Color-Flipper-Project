39768f700f6d80361c567e5d5c5873b88c264854
Jose <jose137sp@gmail.com>
2023-05-09 12:15:15 -0500

    firebase hosting

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDnEqcb06HlE_aDNqRYbTaFe-BwwUzD5a8",
+    authDomain: "color-flipper-js.firebaseapp.com",
+    projectId: "color-flipper-js",
+    storageBucket: "color-flipper-js.appspot.com",
+    messagingSenderId: "926165681803",
+    appId: "1:926165681803:web:ab9bd8eb9724da9d92f68e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 // Seleccionamos el botón y el label
 const button = document.getElementById('btn');
 const body = document.getElementById('body');
